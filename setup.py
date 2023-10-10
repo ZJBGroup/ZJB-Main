3223f1398e1ae28b0ec87fd21b32b41cded5f94f
wangchen <wangchen@zhejianglab.com>
2023-10-10 14:40:47 +0800

    build: 添加setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="zjb-main",
+    version="0.2.0",
+    install_requires=[
+        "zjb-framework>=0.2.0",
+        "numba",
+        "mako",
+        "brainpy",
+        "jaxlib",
+        "matplotlib",
+    ],
+    packages=setuptools.find_packages(),
+)
