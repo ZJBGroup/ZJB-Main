b7dfaf92a337fe0e2298c8779b10d2b3034d49bd
hopeful0 <wangchen@zhejianglab.com>
2023-10-12 17:18:14 +0800

    feat: 部分实体支持从gifti文件创建 (#4)
    * feat: 部分实体支持从gifti文件创建
    
    * fix: 修改sufcace导入时左右脑face数据的合并
    
    ---------
    
    Co-authored-by: wangzhichao <zcwanghz@icloud.com>

--- zjb/main/data/regionmapping.py
+++ zjb/main/data/regionmapping.py
@@ -1,23 +1,52 @@
-from traits.api import Str
+from typing import TYPE_CHECKING
+
+import numpy as np
+from traits.api import Array
 
 from zjb._traits.types import Instance
 from zjb.dos.data import Data
-from zjb.main.data.space import Surface, Volume
+from zjb.main.data.space import SurfaceSpace, VolumeSpace
 from zjb.main.dtb.atlas import Atlas
-from zjb.main.trait_types import IntVector
+from zjb.main.trait_types import RequiredIntVector
+
+if TYPE_CHECKING:
+    from nibabel.gifti.gifti import GiftiImage
 
 
 class SurfaceRegionMapping(Data):
-    surface = Instance(Surface)
+    space = Instance(SurfaceSpace, required=True)
 
-    atlas_name = Str()
+    atlas = Instance(Atlas, required=True)
 
-    data = IntVector
+    data = RequiredIntVector
+
+    @classmethod
+    def from_label_gii(
+        cls,
+        left_label: "GiftiImage | str",
+        right_label: "GiftiImage | str",
+        space: "SurfaceSpace | str",
+        atlas: "Atlas | str",
+    ):
+        from nibabel.gifti.gifti import GiftiImage
+
+        if not isinstance(left_label, GiftiImage):
+            left_label = GiftiImage.from_filename(left_label)
+        if not isinstance(right_label, GiftiImage):
+            right_label = GiftiImage.from_filename(right_label)
+
+        if not isinstance(space, SurfaceSpace):
+            space = SurfaceSpace.from_gii(space, left_label, right_label)
+        if not isinstance(atlas, Atlas):
+            atlas = Atlas.from_label_gii(atlas, left_label)
+        data = np.concatenate([left_label.darrays[0].data, right_label.darrays[0].data])
+
+        return cls(space=space, atlas=atlas, data=data)
 
 
 class VolumeRegionMapping(Data):
-    volume = Instance(Volume)
+    volume = Instance(VolumeSpace, required=True)
 
-    atlas_name = Str()
+    atlas = Instance(Atlas, required=True)
 
-    data = IntVector
+    data = Array(dtype=int, shape=(None, None, None), required=True)
