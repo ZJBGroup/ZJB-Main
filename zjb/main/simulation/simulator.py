27cb6abc74d1d8ad406cd03e3bb35571fb2d3437
wangchen <wangchen@zhejianglab.com>
2023-12-06 13:33:26 +0800

    feat: 仿真器参数支持表达式

--- zjb/main/simulation/simulator.py
+++ zjb/main/simulation/simulator.py
@@ -1,7 +1,6 @@
 from pathlib import Path
-from typing import Any, Callable
+from typing import TYPE_CHECKING, Any, Callable
 
-import numpy as np
 from mako.template import Template
 from traits.api import (
     Dict,
@@ -16,7 +15,7 @@ from traits.api import (
 from zjb._traits.types import Instance
 
 from ..data.correlation import SpaceCorrelation
-from ..dtb.dynamics_model import DynamicsModel
+from ..dtb.dynamics_model import DynamicsModel, HasExpression
 from ..trait_types import FloatVector
 from .monitor import Monitor
 from .solver import EulerSolver, Solver
@@ -26,30 +25,46 @@ TEMPLATE = Template(
 )
 
 
-class Simulator(HasPrivateTraits, HasRequiredTraits):
+class ExprParameter(HasExpression):
+    """表达式参数
+
+    Attributes
+    ----------
+    expression: str
+        Python表达式
+    dependencies: dict[str, float | array[float]], shape (n_regions)
+        表达式依赖的参数字典, 值为参数值
     """
+
+    dependencies = Dict(Str, Union(Float, FloatVector))
+
+
+class Simulator(HasPrivateTraits, HasRequiredTraits):
+    """(脑网络模型)仿真器
+
     Attributes
     ----------
     model : Instance(DynamicsModel)
         动力学模型实例，用于仿真。
-    states : Dict(Str, Union(Float, FloatVector))
-        仿真的状态变量。
-    parameters : Dict(Str, Union(Float, FloatVector))
-        动力学模型的参数。
-    connectivity : Instance(SpaceCorrelation)
-        空间相关性连接的实例，描述不同节点间的连接性。
-    solver : Instance(Solver)
+    states : dict[str, float | array[float]], shape (n_regions)
+        仿真器当前的状态变量字典
+    parameters : dict[str, ExprParameter | float | array[float]], shape (n_regions)
+        仿真器当前的参数字典
+    connectivity : array_like, shape (n_regions, n_regions)
+        脑网络的连接矩阵
+    solver : Solver
         求解器实例，用于数值求解仿真的微分方程。
     monitors : List(Instance(Monitor))
         监视器列表，用于在仿真过程中采样数据。
     t : Float
         仿真总时长。
     """
+
     model = Instance(DynamicsModel, required=True)
 
     states = Dict(Str, Union(Float, FloatVector))
 
-    parameters = Dict(Str, Union(Float, FloatVector))
+    parameters = Dict(Str, Union(Instance(ExprParameter), Float, FloatVector))
 
     connectivity = Instance(SpaceCorrelation, required=True)
 
@@ -68,28 +83,22 @@ class Simulator(HasPrivateTraits, HasRequiredTraits):
         for name, parameter in self.model.parameters.items():
             self.parameters.setdefault(name, parameter)
 
-        self._args = (
-            {
-                "__t": self.t,
-                "__dt": self.solver.dt,
-                "__C": self.connectivity.data,
-            }
-            | self.states
-            | self.parameters
-        )
+        self._args = {
+            "__t": self.t,
+            "__dt": self.solver.dt,
+            "__C": self.connectivity.data,
+        } | self.states
 
-        self._env: dict[str, Any] = {}
+        self._env: dict[str, Any] = self._update_env_from_parameter({})
         self._code = TEMPLATE.render(  # type: ignore
-            solver=self.solver, monitors=self.monitors, model=self.model, env=self._env
+            simulator=self,
+            solver=self.solver,
+            monitors=self.monitors,
+            model=self.model,
+            env=self._env,
         )
         exec(self._code, self._env)
-        self._simulator: Callable[
-            ...,
-            tuple[
-                tuple[np.ndarray[Any, Any], ...],
-                tuple[np.ndarray[Any, Any], ...],
-            ],
-        ] = self._env["simulator"]
+        self._simulator: "SimulatorFunction" = self._env["simulator"]
 
     def __call__(self):
         """
@@ -108,3 +117,29 @@ class Simulator(HasPrivateTraits, HasRequiredTraits):
             self.states[name] = state
 
         return results
+
+    def _update_env_from_parameter(self, env: "dict[str, Any]"):
+        """根据仿真器参数更新编译所需的环境变量"""
+        for name, para in self.parameters.items():
+            if not isinstance(para, ExprParameter):
+                env[name] = para
+                continue
+            for dname, dependency in para.dependencies.items():
+                if dname in env:
+                    raise KeyError(
+                        f"Dependency {dname} of parameter {name} is duplicated"
+                    )
+                env[dname] = dependency
+        return env
+
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
+
+    SimulatorFunction = Callable[
+        ...,
+        tuple[
+            tuple[NDArray[Any], ...],
+            tuple[NDArray[Any], ...],
+        ],
+    ]
