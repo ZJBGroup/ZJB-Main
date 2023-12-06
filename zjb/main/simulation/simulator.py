4ec232694dad7f1d6be3240650f5d116ebcf3714
wangchen <wangchen@zhejianglab.com>
2023-12-06 20:36:54 +0800

    feat: 新增Numba函数参数类型, 表达式参数继承该类型

--- zjb/main/simulation/simulator.py
+++ zjb/main/simulation/simulator.py
@@ -1,21 +1,26 @@
+from enum import StrEnum
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, Callable
 
 from mako.template import Template
+from traits.api import Dict
+from traits.api import Enum as TraitEnum
 from traits.api import (
-    Dict,
+    Expression,
     Float,
     HasPrivateTraits,
     HasRequiredTraits,
     List,
+    Property,
     Str,
     Union,
+    property_depends_on,
 )
 
-from zjb._traits.types import Instance
+from zjb._traits.types import Instance, TraitCallable
 
 from ..data.correlation import SpaceCorrelation
-from ..dtb.dynamics_model import DynamicsModel, HasExpression
+from ..dtb.dynamics_model import DynamicsModel
 from ..trait_types import FloatVector
 from .monitor import Monitor
 from .solver import EulerSolver, Solver
@@ -24,19 +29,79 @@ TEMPLATE = Template(
     filename=str(Path(__file__).parent / "_templates" / "simulator.mako")
 )
 
+_EXPR_TEMPLATE = Template(
+    filename=str(Path(__file__).parent / "_templates" / "expr.mako")
+)
 
-class ExprParameter(HasExpression):
+
+class BUILTIN_PARAMETER(StrEnum):
+    """内建参数, 仿真器内部预先定义的一些参数, 可以在表达式中直接使用这些参数"""
+
+    t = "__t"
+    """仿真总时长"""
+    dt = "__dt"
+    """仿真时间步长, 由求解器定义"""
+    C = "__C"
+    """脑网络的连接矩阵"""
+    C_1 = "__C_1"
+    """脑网络连接矩阵的行求和, 等于`np.sum(__C, 1)`"""
+    nt = "__nt"
+    """仿真的总时间步数, 等于`int(__t / __dt)`"""
+    nr = "__nr"
+    """脑网络的节点数, 等于`__C.shape[0]`"""
+    it = "__it"
+    """仿真当前迭代的时间步, 整数, 从0到__nt-1"""
+    ct = "__ct"
+    """仿真当前迭代的时间, 等于`__dt * __it`"""
+
+
+class NumbaFuncParameter(HasPrivateTraits, HasRequiredTraits):
+    """Numba函数参数
+
+    Attributes
+    ----------
+    dependencise: list[BUILTIN_PARAMETER]
+        函数所依赖的内建参数, by default (,)
+    numba_func: Callable[..., float | array[float]]
+        `numba.njit`装饰的可执行函数
+    """
+
+    dependencies = List(TraitEnum(BUILTIN_PARAMETER))
+
+    numba_func: "Callable[..., float | NDArray[float_]]" = TraitCallable(required=True)
+
+    _arg_str: str = Property()
+
+    @property_depends_on("dependencies")
+    def _get__arg_str(self):
+        return ",".join(self.dependencies)
+
+
+class ExprParameter(NumbaFuncParameter):
     """表达式参数
 
     Attributes
     ----------
     expression: str
         Python表达式
-    dependencies: dict[str, float | array[float]], shape (n_regions)
-        表达式依赖的参数字典, 值为参数值
+    parameters: dict[str, float | array[float]], shape (n_regions)
+        表达式内部的参数字典
+    numba_func: Callable[..., float | array[float]], 只读
+        numba装饰的可执行函数
     """
 
-    dependencies = Dict(Str, Union(Float, FloatVector))
+    expr = Expression()
+
+    parameters = Dict(Str, Union(Float, FloatVector))
+
+    numba_func: "Callable[..., float | NDArray[float_]]" = Property()
+
+    @property_depends_on(["expression", "dependencies", "parameters"])
+    def _get_numba_func(self):
+        code = _EXPR_TEMPLATE.render(expr=self)
+        env = self.parameters.copy()
+        exec(code, env)
+        return env["func"]
 
 
 class Simulator(HasPrivateTraits, HasRequiredTraits):
@@ -64,7 +129,7 @@ class Simulator(HasPrivateTraits, HasRequiredTraits):
 
     states = Dict(Str, Union(Float, FloatVector))
 
-    parameters = Dict(Str, Union(Instance(ExprParameter), Float, FloatVector))
+    parameters = Dict(Str, Union(Instance(NumbaFuncParameter), Float, FloatVector))
 
     connectivity = Instance(SpaceCorrelation, required=True)
 
@@ -121,25 +186,21 @@ class Simulator(HasPrivateTraits, HasRequiredTraits):
     def _update_env_from_parameter(self, env: "dict[str, Any]"):
         """根据仿真器参数更新编译所需的环境变量"""
         for name, para in self.parameters.items():
-            if not isinstance(para, ExprParameter):
+            if isinstance(para, NumbaFuncParameter):
+                env[f"_{name}_func"] = para.numba_func
+            else:
                 env[name] = para
-                continue
-            for dname, dependency in para.dependencies.items():
-                if dname in env:
-                    raise KeyError(
-                        f"Dependency {dname} of parameter {name} is duplicated"
-                    )
-                env[dname] = dependency
         return env
 
 
 if TYPE_CHECKING:
+    from numpy import float_
     from numpy.typing import NDArray
 
     SimulatorFunction = Callable[
         ...,
         tuple[
-            tuple[NDArray[Any], ...],
-            tuple[NDArray[Any], ...],
+            tuple[NDArray[float_], ...],
+            tuple[NDArray[float_], ...],
         ],
     ]
